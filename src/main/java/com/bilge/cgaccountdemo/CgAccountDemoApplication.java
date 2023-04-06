2f60742f71afd8dc8be5c72212b76c8b07a6f411
Bilge Güç <bilgeguc@gmail.com>
2023-04-06 17:58:11 +0300

    master/
    First springboot init.

---
+++ src/main/java/com/bilge/cgaccountdemo/CgAccountDemoApplication.java
@@ -0,0 +1,13 @@
+package com.bilge.cgaccountdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgAccountDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgAccountDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bilge/cgaccountdemo/CgAccountDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bilge.cgaccountdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CgAccountDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
